abaea6076572794e183657826b84a3fee4aafa07
okamoto <okamoto_yu-ki@outlook.jp>
2021-10-07 22:47:55 +0900

    cliを作ってみた

---
+++ part2/samplecli/src/rpn_calculator.rs
@@ -0,0 +1,11 @@
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        0
+    }
+}
